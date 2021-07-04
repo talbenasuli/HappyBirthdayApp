5dac14685a3e86c822f647b7698a7e375fd4be29
Tal Ben Asuli MAC <talbapepper@gmail.com>
2021-07-04 14:38:29 +0300

    Initial Commit

---
+++ HappyBirthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyBirthdayApp
+//
+//  Created by Tal Ben Asuli MAC  on 04/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
